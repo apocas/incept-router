1e710b005ea0d17a591c51bc8a12d6ca9bcb425e
apocas <petermdias@gmail.com>
2013-09-22 05:35:24 +0100

    stable

---
+++ main.js
@@ -0,0 +1,22 @@
+var payloads = require('nano')('http://localhost:5984').db.use('payloads'),
+  redis = require('redis');
+
+rclient = redis.createClient();
+
+payloads.list(function(err, body) {
+  body.rows.forEach(function(doc) {
+    payloads.get(doc.id, {revs_info: false}, function(err, body) {
+      console.log('Loading... ' + body.domain);
+
+      rclient.lpop('frontend:' + body.domain, function(err, data) {
+        rclient.multi()
+          .del('frontend:' + body.domain, body.domain)
+          .rpush('frontend:' + body.domain, body.domain)
+          .rpush('frontend:' + body.domain, 'http://' + body.node + ':' + body.docker.NetworkSettings.PortMapping.Tcp['80'])
+          .exec(function(err, replis) {
+            process.exit(0);
+          });
+      });
+    });
+  });
+});
